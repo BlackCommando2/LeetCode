166a24e4ebdabcb020dac4220515b5c0cf88ba6e
BlackCommando2 <75882787+BlackCommando2@users.noreply.github.com>
2023-08-29 15:54:08 +0530

    Time: 52 ms (63.94%), Space: 13.5 MB (24.54%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+        start_color = image[sr][sc]
+        
+        def flood_fill(x, y):
+            if x < 0 or x >= len(image): return
+            if y < 0 or y >= len(image[0]): return
+            
+            if image[x][y] == color: return
+            if image[x][y] != start_color: return
+            
+            image[x][y] = color
+            
+            flood_fill(x-1, y)
+            flood_fill(x+1, y)
+            flood_fill(x, y+1)
+            flood_fill(x, y-1)
+        
+        flood_fill(sr, sc)
+        return image
